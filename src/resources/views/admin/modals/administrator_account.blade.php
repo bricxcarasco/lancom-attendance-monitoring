1e5587b122537deed3fe4a0d6dea296785ea60c1
Bricx Carasco <bricxraincarasco21@gmail.com>
2020-08-10 15:32:33 +0800

    Modify admin account ui

---
+++ src/resources/views/admin/modals/administrator_account.blade.php
@@ -0,0 +1,69 @@
+<div class="modal fade" id="add-account-modal">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title"><i class="fas fa-user"></i> Add Administrator Account</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+
+                <div class="form-group">
+                    <label>Name</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="far fa-user"></i></span>
+                        </div>
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label>Email address</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                        </div>
+                        <input type="email" class="form-control">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label>Birthday</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-birthday-cake"></i></span>
+                        </div>
+                        <input type="date" class="form-control" data-inputmask-alias="datetime" data-inputmask-inputformat="mm/dd/yyyy" data-mask>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label>Address</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-map-marked-alt"></i></span>
+                        </div>
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label>Contact Number</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-phone"></i></span>
+                        </div>
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
+
+            </div>
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Add Account</button>
+            </div>
+        </div>
+    </div>
+</div>
--- src/resources/views/admin/users/administrator.blade.php
+++ src/resources/views/admin/users/administrator.blade.php
@@ -1,6 +1,5 @@
 @include('templates.header')
 
-
 <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
 <link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
 
@@ -28,6 +27,10 @@
                     <h3 class="card-title">Account List</h3>
                     
                     <div class="card-tools">
+                        <button type="button" class="btn btn-sm btn-warning btn-add-account" data-toggle="modal" data-target="#add-account-modal">
+                            <i class="fas fa-user-circle"></i> Add Account
+                        </button>
+
                         <button type="button" class="btn btn-tool" data-card-widget="collapse">
                             <i class="fas fa-minus"></i>
                         </button>
@@ -41,7 +44,7 @@
                                 <th>Name</th>
                                 <th>Email</th>
                                 <th>Created At</th>
-                                <th>Created Date</th>
+                                <th>Account Age</th>
                                 <th>Status</th>
                                 <th>Action</th>
                             </tr>
@@ -52,9 +55,24 @@
                                     <td>{{ $user->name }}</td>
                                     <td>{{ $user->email }}</td>
                                     <td>{{ $user->created_at }}</td>
-                                    <td>{{ $user->created_at }}</td>
-                                    <td>{{ $user->is_delete }}</td>
-                                    <td>{{ $user->is_delete }}</td>
+                                    <td>{{ $user->created_at->diffForHumans() }}</td>
+                                    <td>
+                                        @if ($user->is_active === 0)
+                                            <span class="badge badge-pill badge-success">Active</span>
+                                        @else
+                                            <span class="badge badge-pill badge-secondary">Inactive</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <button class="btn btn-sm btn-primary btn-edit-account"><i class="fas fa-user-edit"></i> Edit</button>
+                                        @if ($user->is_active === 0)
+                                            <button class="btn btn-sm btn-success btn-change-password-account"><i class="fas fa-edit"></i> Change Password</button>
+                                            <button class="btn btn-sm btn-warning btn-disable-account"><i class="fas fa-thumbs-down"></i> Disable</button>
+                                        @else
+                                            <button class="btn btn-sm btn-success btn-enable-account"><i class="fas fa-thumbs-up"></i> Enable</button>
+                                        @endif
+                                        <button class="btn btn-sm btn-danger btn-delete-account"><i class="fas fa-trash"></i> Delete</button>
+                                    </td>
                                 </tr>
                             @endforeach
                         </tbody>
@@ -63,7 +81,7 @@
                                 <th>Name</th>
                                 <th>Email</th>
                                 <th>Created At</th>
-                                <th>Created Date</th>
+                                <th>Account Age</th>
                                 <th>Status</th>
                                 <th>Action</th>
                             </tr>
@@ -75,14 +93,13 @@
     </section>
 </div>
 
-
+@include('admin.modals.administrator_account')
 @include('templates.footer')
 
 <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
 <script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
 <script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
 <script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
-
 <script src="{{ asset('js/admin/userAdministrator.js') }}"></script>
 
 @include('templates.footer-end')
